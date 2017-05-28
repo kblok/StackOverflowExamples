44798359011b4ea1aa011e1876a4aeaa8a2f1925
Dario Kondratiuk <dariokondratiuk@gmail.com>
2017-05-28 13:41:33 -0300

    Route demo

---
+++ AspNetDemoProject/AspNetDemoProject/App_Code/ErrorModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetDemoProject.App_Code
+{
+	public class ErrorModule : IHttpModule
+	{
+		public void Init(HttpApplication app)
+		{
+			app.Error += OnError;
+		}
+
+		public void OnError(object obj, EventArgs args)
+		{
+			// At this point we have information about the error
+			var ctx = HttpContext.Current;
+		}
+
+		public void Dispose() { }
+	}
+}
---
+++ AspNetDemoProject/AspNetDemoProject/Demos/routingDemo.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNetDemoProject.Demos
+{
+	public partial class routingDemo : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Response.Write(Request.Url.ToString().Length);
+			Response.Write("<br/>");
+			Response.Write(Request.QueryString["url"]);
+			/*
+			if (!string.IsNullOrEmpty(Request.QueryString["url"])){
+				Response.Redirect("/Demos/routingDemo.aspx?finalURL=" + Request.QueryString["url"].Replace("route", ""), false);
+			}*/
+		}
+	}
+}
